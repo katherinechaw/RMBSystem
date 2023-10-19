d1ca3be06d6fee3d15f7f27d1b9d58469b0612ae
chawmitsui-soko <125328479+chawmitsui-soko@users.noreply.github.com>
2023-10-19 09:23:45 +0800

    Add files via upload

---
+++ RecordManagementPortalDev/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function toggle(id) {
+    var state = document.getElementById(id).style.display;
+    if (state == 'block') {
+        document.getElementById(id).style.display = 'none';
+    } else {
+        document.getElementById(id).style.display = 'block';
+    }
+}
+
